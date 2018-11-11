c8049dcb659e2903810255e0325e5de0ea95b9d0
Alex Shinkevich <alex.shinkevich@gmail.com>
2018-11-12 02:38:44 +0300

    Some changes to push package to PyPi

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='nameko-mongodb',
-    version='0.0.2-dev1',
-    description='Simple MongoDb dependency for nameko microservices',
-    author='saiqi',
-    author_email='julien.bernard.iphone@gamil.com',
-    url='http://github.com/saiqi/nameko-mongodb',
+    version='0.0.1-dev',
+    description='Simple MongoDb dependency for nameko (microservices framework). Based on https://github.com/saiqi/nameko-mongodb',
+    author='Alex Shinkevich',
+    author_email='alex.shinkevich@gmail.com',
+    url='https://github.com/alexshin/nameko-mongodb',
     packages=['nameko_mongodb'],
     install_requires=[
         'nameko>=2.11.0',
@@ -20,9 +20,12 @@ setup(
     license='Apache License, Version 2.0',
     classifiers=[
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Intended Audience :: Developers",
