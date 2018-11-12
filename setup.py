7372d20b900edb6000b8cefcc2f56bb2f2232603
Alex Shinkevich <alex.shinkevich@gmail.com>
2018-11-12 06:50:46 +0300

    Changed version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='nameko-mongodb',
-    version='1.0.4',
+    version='1.1.0',
     description='Simple MongoDb dependency for nameko (microservices framework). '
                 'Based on https://github.com/saiqi/nameko-mongodb',
     long_description=long_description,
